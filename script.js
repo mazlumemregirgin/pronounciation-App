d64ae8b9dec8aea9c6bb2337d00629047beca78f
Mazlum Emre Girgin <mazlumemregirgin@msn.com>
2023-08-31 00:13:00 +0300

    json file fetched

--- script.js
+++ script.js
@@ -1,92 +1,95 @@
-const wordName = document.querySelector("#word-name")
-const phonetic = document.querySelector("#phonetic-info")
-const meaning = document.querySelector("#meaning-info")
-const targetAudio = document.querySelector("#target-audio")
+const wordName = document.querySelector("#word-name");
+const phoneticEng = document.querySelector("#phonetic-info-eng");
+const phoneticUsa = document.querySelector("#phonetic-info-us");
+const meaning = document.querySelector("#meaning-info");
+const targetAudioEng = document.querySelector("#target-audio-eng");
+const targetAudioUsa = document.querySelector("#target-audio-us");
 
+const button = document.getElementById("change-button");
 
-const apiUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
-const button = document.getElementById('change-button');
+button.addEventListener("click", function () {
+  fetch("\\words.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const arrayLength = data.length;
+      const randomIndex = Math.floor(Math.random() * arrayLength);
 
-button.addEventListener('click', () => {
-  fetchRandomWord();
+      wordName.innerText = data[randomIndex].word;
+      meaning.innerText = data[randomIndex].definition;
+      phoneticEng.innerText =
+        "English :   " + data[randomIndex].phonetics[0].eng;
+      phoneticUsa.innerText =
+        "American :   " + data[randomIndex].phonetics[0].us;
+      targetAudioEng.src = data[randomIndex].audio[0].eng;
+      targetAudioUsa.src = data[randomIndex].audio[0].us;
+    })
+    .catch((error) => console.error("Fetch error:", error));
 });
 
-function fetchRandomWord() {
-  // Rasgele bir kelime seçme
-  const randomWords = [ "apple","apartment","angel","aspect","alien","alley","accident","acoustic","congratulations","actor","ambition","accent","accident","account","accusation","achievement","witness","worker","world","wrap","writer","writing","year","yellow","yield","young","youth",];
-  const randomIndex = Math.floor(Math.random() * randomWords.length);
-  const randomWord = randomWords[randomIndex];
-
-  const wordUrl = apiUrl + randomWord;
-  
-  fetch(wordUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('HTTP error ' + response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      wordName.innerText=data[0].word;
-      phonetic.innerText=data[0].phonetic
-      meaning.innerText=data[0].meanings[0].definitions[0].definition;
-      targetAudio.src=data[0].phonetics[0].audio;
-    })
-    .catch(error => {
-      console.error('Veri çekme hatası:', error);
-      fetchRandomWord()
-    });
+const recordButton = document.getElementById("recordButton");
+const stopButton = document.getElementById("stopButton");
+const audio = document.getElementById("user-audio");
+let mediaRecorder;
+let recordedChunks = [];
 
 
-    
+navigator.mediaDevices.getUserMedia({ audio: true }).then(function (stream) {
+  mediaRecorder = new MediaRecorder(stream);
 
-  
-  
-}
+  mediaRecorder.ondataavailable = (event) => {
+    if (event.data.size > 0) {
+      recordedChunks.push(event.data);
+    }
+  };
 
+  mediaRecorder.onstop = () => {
+    const blob = new Blob(recordedChunks, { type: "audio/wav" });
+    recordedChunks = [];
+    audio.src = URL.createObjectURL(blob);
+  };
+});
 
+recordButton.addEventListener("click", () => {
+  mediaRecorder.start();
+  recordButton.disabled = true;
+  stopButton.disabled = false;
+});
 
+stopButton.addEventListener("click", () => {
+  mediaRecorder.stop();
+  recordButton.disabled = false;
+  stopButton.disabled = true;
+});
 
+//const apiUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+//button.addEventListener('click', () => {
+//   fetchRandomWord();
+// });
 
+// function fetchRandomWord() {
+//   const randomWords = [ "wonderful","resolution","quit","target","disagree","disappear","dislike","misplace","misunderstand","overpopulated","overwork","rebuild","rethink","underpaid","pressure","reliable","skill","management","technology","advise","allow","encourage","persuade","attract","customer"]
+//   const randomIndex = Math.floor(Math.random() * randomWords.length);
+//   const randomWord = randomWords[randomIndex];
 
+//   const wordUrl = apiUrl + randomWord;
 
+//   fetch(wordUrl)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error('HTTP error ' + response.status);
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       console.log(JSON.stringify(data, null,2 ))
+//       wordName.innerText=data[0].word;
+//       phoneticEng.innerText= "English :   " + data[0].phonetic;
+//       phoneticUsa.innerText= "American :   " + data[0].phonetic;
+//       meaning.innerText=data[0].meanings[0].definitions[0].definition;
 
+//     })
+//     .catch(error => {
+//       console.error('Veri çekme hatası:', error);
 
-
-
-
-    const recordButton = document.getElementById('recordButton');
-    const stopButton = document.getElementById('stopButton');
-    const audio = document.getElementById('user-audio');
-    let mediaRecorder;
-    let recordedChunks = [];
-
-    // Kullanıcı izinlerini kontrol et
-    navigator.mediaDevices.getUserMedia({ audio: true })
-        .then(function(stream) {
-            mediaRecorder = new MediaRecorder(stream);
-
-            mediaRecorder.ondataavailable = event => {
-                if (event.data.size > 0) {
-                    recordedChunks.push(event.data);
-                }
-            };
-
-            mediaRecorder.onstop = () => {
-                const blob = new Blob(recordedChunks, { type: 'audio/wav' });
-                recordedChunks = [];
-                audio.src = URL.createObjectURL(blob);
-            };
-        });
-
-    recordButton.addEventListener('click', () => {
-        mediaRecorder.start();
-        recordButton.disabled = true;
-        stopButton.disabled = false;
-    });
-
-    stopButton.addEventListener('click', () => {
-        mediaRecorder.stop();
-        recordButton.disabled = false;
-        stopButton.disabled = true;
-    });
+//     });
+// }
